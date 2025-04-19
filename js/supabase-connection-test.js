567c2871a5a8495d5ef79696432478ee58721c18
Jason Golden <jasonwilliamgolden@gmail.com>
2025-04-19 07:40:09 -0400

    supabase connect

--- js/supabase-connection-test.js
+++ js/supabase-connection-test.js
@@ -2,7 +2,7 @@
 // Place this in your main HTML page or a JS file that loads on your public site for quick verification
 
 const supabaseUrl = 'https://cbopynuvhcymbumjnvay.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNib3B5bnV2aGN5bWJ1bWpuVmF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg2NjYzMDMsImV4cCI6MjAwNDI0MjMwM30.4y6Lk9mTQJ9dGvFz9Kp8F5qJ7Yk5F2w8zQJQ9gBf2bA';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNib3B5bnV2aGN5bWJ1bWpudmF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5OTUxNzMsImV4cCI6MjA1OTU3MTE3M30.UZElMkoHugIt984RtYWyfrRuv2rB67opQdCrFVPCfzU';
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 // Try to fetch a single blog row to test connection
